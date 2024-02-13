095a16d4b83d49a153164fa9b113f32a801a8f41
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-02-13 16:59:52 -0500

    Installed graphql, express-graphql packages and established the middleware graphqlHTTP

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express=require('express');
+const graphqlHTTP=require('express-graphql');
+
 const app=express();
+app.use("/graphql",graphqlHTTP({
+
+}));
 
 app.listen(4000,(res)=>{
     console.log("Listening to server on Port 4000");
