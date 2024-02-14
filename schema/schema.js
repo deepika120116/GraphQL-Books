87f1fc4e4962755fb4a69cb60eb59eb3fff6d52b
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-02-13 20:18:06 -0500

    Writing resolve function to search book by id and installed lodash to find the book from dummy data

--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,15 @@
 const graphql=require('graphql');
+const _=require('lodash');
 
 const {GraphQLObjectType,GraphQLID,GraphQLString,GraphQLSchema}=graphql;
 
+
+//dummy books
+const books=[
+    {name:'Name of the Wind',genre:"Fantasy",id:1},
+    {name:'The Final Empire',genre:"Fantasy",id:2},
+    {name:'The Long Earth',genre:"Sci-Fi",id:3}
+];
 const BookType=new GraphQLObjectType({
     name:"Book",
     fields:()=>({
@@ -19,6 +27,7 @@ const RootQuery=new GraphQLObjectType({
             args:{id:{type:GraphQLID}},
             resolve(parent,args){
                 //Code to connect to DB, parent is used for Relation connection
+                return _.find(books,{id:args.id});
             }
         }
     }
