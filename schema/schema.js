9c108e3fa195d890a8b498a4706b8643b85378fc
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-02-14 13:39:08 -0500

    Added authortype and resolve function for Author rootquery

--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,11 @@
 const graphql=require('graphql');
 const _=require('lodash');
 
-const {GraphQLObjectType,GraphQLID,GraphQLString,GraphQLSchema}=graphql;
+const {GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema}=graphql;
 
 
 //dummy books
@@ -10,12 +14,27 @@ const books=[
     {name:'The Final Empire',genre:"Fantasy",id:"2"},
     {name:'The Long Earth',genre:"Sci-Fi",id:"3"}
 ];
+
+const authors=[
+    {name:"Patrick Rothfuss", age:44,id:"1"},
+    {name:"Brandon Sanderson", age:42,id:"2"},
+    {name:"Terry Pratchett", age:66,id:"3"},
+]
 const BookType=new GraphQLObjectType({
     name:"Book",
     fields:()=>({
         id:{type:GraphQLID},
         name:{type:GraphQLString},
         genre:{type:GraphQLString}
+    })
+});
+
+const AuthorType=new GraphQLObjectType({
+    name:"Author",
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        age:{type:GraphQLInt}
     })
 });
 
@@ -28,6 +47,13 @@ const RootQuery=new GraphQLObjectType({
             resolve(parent,args){
                 //Code to connect to DB, parent is used for Relation connection
                 return _.find(books,{id:args.id});
+            }
+        },
+        author:{
+            type:AuthorType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){
+                return _.find(authors,{id:args.id});
             }
         }
     }
