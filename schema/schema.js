4604b28d7b6af678e06ee8d26857f5c4555a7fde
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-02-14 13:10:50 -0500

    destructured {grapghqlHTTP} from express-graphql

--- schema/schema.js
+++ schema/schema.js
@@ -6,9 +6,9 @@ const {GraphQLObjectType,GraphQLID,GraphQLString,GraphQLSchema}=graphql;
 
 //dummy books
 const books=[
-    {name:'Name of the Wind',genre:"Fantasy",id:1},
-    {name:'The Final Empire',genre:"Fantasy",id:2},
-    {name:'The Long Earth',genre:"Sci-Fi",id:3}
+    {name:'Name of the Wind',genre:"Fantasy",id:"1"},
+    {name:'The Final Empire',genre:"Fantasy",id:"2"},
+    {name:'The Long Earth',genre:"Sci-Fi",id:"3"}
 ];
 const BookType=new GraphQLObjectType({
     name:"Book",
