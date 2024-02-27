522fcaec8d8563fb428f54d5652c07ecf93515ff
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-02-27 14:57:30 -0500

    Connection established to database

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express=require('express');
 const {graphqlHTTP}=require('express-graphql');
+const mongoose=require('mongoose');
 const schema=require('./schema/schema');
 
 const app=express();
+
+mongoose.connect("mongodb://localhost:27017/graphql");
+mongoose.connection.once("open",()=>{
+    console.log("Connected to Database");
+});
 app.use("/graphql",graphqlHTTP({
 schema,
 graphiql:true
