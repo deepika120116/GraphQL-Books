40fde0bb7286d8df1d9238937520e720f91d3da8
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-02-28 13:22:45 -0500

    Added the mutation for addAuthor

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express=require('express');
 const {graphqlHTTP}=require('express-graphql');
 const mongoose=require('mongoose');
 const schema=require('./schema/schema');
-const book=require('../server/models/book');
-const author=require('../server/models/author');
+
 
 const app=express();
 
--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,7 @@
 const graphql=require('graphql');
 const _=require('lodash');
+const Book=require("../models/book");
+const Author=require('../models/author');
 
 const {GraphQLObjectType,
     GraphQLID,
@@ -71,6 +73,26 @@ const RootQuery=new GraphQLObjectType({
     }
 });
 
+const Mutation=new GraphQLObjectType({
+    name:"Mutation",
+    fields:{
+        addAuthor:{
+            type:AuthorType,
+            args:{
+                name:{type:GraphQLString},
+                age:{type:GraphQLInt}
+            },
+            resolve(parent,args){
+                let author=new Author({
+                    name:args.name,
+                    age:args.age
+                });
+                return author.save();
+            }
+        }
+    }
+})
 module.exports=new GraphQLSchema({
-    query:RootQuery
+    query:RootQuery,
+    mutation:Mutation
 })
