a13975ffc94da73a99a68e404bdd905b4cfceaa2
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-02-29 12:54:27 -0500

    installed @apollo/clinet and graphql and acquired apollo clinet, provider and  inmemory cache and wrapped it around APP component

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import BookList from "./components/BookList"
+
+function App() {
+
+  return (
+    <>
+     <h1>Deepika's Book List</h1>
+     <BookList/>
+    </>
+  )
+}
+export default App;
---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,10 @@
+const BookList=()=>{
+    return(
+        <>
+            <ul id="book-list">
+                <li>Book Name</li>
+            </ul>
+        </>
+    );
+};
+export default BookList;
